a25c292386da112e1d467fbe8b153ade5f0ae37d
Ilya Androsov <ilya_androsov@icloud.com>
2020-02-12 01:21:10 +0300

    feat: add new game functionality

--- src/App.js
+++ src/App.js
@@ -13,51 +13,77 @@ class App extends Component {
   state = {
     score: 0,
     round: 0,
+    roundScore: 5,
     secretBird: this.setRandomBirdIndex(),
     selectedBird: null,
     isRoundFinished: false,
     isGameFinished: false,
     secretSong: null,
-    selectedSong: null
+    selectedSong: null,
+    secretImage: null,
+    selectedImage: null
   }
 
   constructor(props) {
     super(props);
     this.setSecretBirdSound();
+    // this.setSecretBirdImage();
   }
 
   startNewGame = () => {
     this.setState({
       score: 0,
       round: 0,
+      roundScore: 5,
+      secretBird: this.setRandomBirdIndex(),
       selectedBird: null,
       isRoundFinished: false,
       isGameFinished: false,
-      selectedSong: null
-    });
-    this.setRandomBirdIndex();
+      selectedSong: null,
+      secretImage: null,
+      selectedImage: null
+    }, this.setSecretBirdSound());
+    this.clearListIndicators();
   }
 
   setRandomBirdIndex() {
-    return Math.floor(Math.random() * 7);
+    return Math.floor(Math.random() * 6);
   }
 
   increaseScoreBy = (value) => {
-    this.setState((state, value) => ({
-      score: state.counter + value
+    console.log(value);
+    this.setState((state) => ({
+      score: state.score + value
+    }));
+  }
+
+  decreaseRoundScore = () => {
+    this.setState((state) => ({
+      roundScore: state.roundScore - 1
     }));
   }
 
   setNextRound = () => {
-    this.setState((state) => ({
-      round: state.round + 1,
-      selectedBird: null,
-      isGameFinished: state.round === birdsData.length
-    }));
+    if (this.state.isRoundFinished){
+      this.setState((state) => ({
+        round: state.round + 1,
+        roundScore: 5,
+        selectedBird: null,
+        secretBird: this.setRandomBirdIndex(),
+        isRoundFinished: false,
+        isGameFinished: state.round === 4,
+        secretSong: null,
+        selectedSong: null,
+        secretImage: null,
+        selectedImage: null
+      }), () => this.setSecretBirdSound());
+      this.clearListIndicators();
+    }
   }
 
-  setSecretBirdSound() {
+  setSecretBirdSound = () => {
     const latinName = birdsData[this.state.round].themeList[this.state.secretBird].latinName;
+    console.log('Правильный ответ: ', this.state.secretBird, latinName);
     const proxy = 'https://cors-anywhere.herokuapp.com/';
     fetch(`${proxy}https://www.xeno-canto.org/api/2/recordings?query=${latinName}`)
       .then(response => response.json())
@@ -68,7 +94,8 @@ class App extends Component {
       })
   }
 
-  setSelectedBirdSound = (latinName) => {
+  setSelectedBirdSound = () => {
+    const latinName = birdsData[this.state.round].themeList[this.state.selectedBird].latinName;
     const proxy = 'https://cors-anywhere.herokuapp.com/';
     fetch(`${proxy}https://www.xeno-canto.org/api/2/recordings?query=${latinName}`)
       .then(response => response.json())
@@ -79,14 +106,57 @@ class App extends Component {
       })
   }
 
+  checkAnswer = (event, answer) => {
+    const target = event.target.closest('li');
+    const checkedId = target.id;
+    if (!this.state.isRoundFinished) {
+      if (checkedId === String(answer)) {
+        this.setState((state) => ({
+          isRoundFinished: true
+        }));
+        this.increaseScoreBy(this.state.roundScore)
+        target.classList.add('right');
+      } else {
+        target.classList.add('false');
+        this.decreaseRoundScore();
+      }
+    }
+    this.setState((state) => ({
+      selectedBird: checkedId,
+      selectedSong: null,
+    }), this.setSelectedBirdSound);
+  }
+
+  clearListIndicators = () => {
+    document.querySelectorAll('.answer-item').forEach(element => {
+      element.classList.remove('false');
+      element.classList.remove('right');
+    });
+  }
+
+  setSecretBirdImage = () => {
+    const latinName = birdsData[this.state.round].themeList[this.state.secretBird].latinName;
+    const API_KEY = '7cca3ab415383e79'
+    const URL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&apikey=${API_KEY}&tagmode=all&extras=urlm&format=json&nojsoncallback&tags=${latinName}`;
+    console.log(URL);
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // this.setState((state) => ({
+        //   selectedSong: data.recordings[0].file
+        // }));
+      })
+  }
+
   render() {
     return (
       <Fragment>
         <Header scoreValue={this.state.score}/>
         <ThemesList round={this.state.round} birdsData={birdsData}/>
         <QuestionDescription isRoundFinished={this.state.isRoundFinished} birdElement={birdsData[this.state.round].themeList[this.state.secretBird]} secretSong={this.state.secretSong}/>
-        <AnswerList answersArray={birdsData[this.state.round].themeList} answer={this.state.secretBird}/>
-        <DescriptionBlock birdElement={birdsData[this.state.round].themeList[this.state.selectedBird]}/>
+        <AnswerList answersArray={birdsData[this.state.round].themeList} answer={this.state.secretBird} checkAnswer={this.checkAnswer}/>
+        <DescriptionBlock birdElement={birdsData[this.state.round].themeList[this.state.selectedBird]} selectedSong={this.state.selectedSong}/>
         <NextLevelButton clickFunction={this.state.isGameFinished ? this.startNewGame : this.setNextRound} enabled={this.state.isRoundFinished}/>
       </Fragment>
     );
--- src/components/AnswerList/AnswerList.jsx
+++ src/components/AnswerList/AnswerList.jsx
@@ -2,14 +2,7 @@ import React from 'react';
 import AnswerItem from '../AnswerItem/AnswerItem'
 import './AnswerList.scss'
 
-function checkAnswer(event, answer) {
-  console.log(answer);
-  if (event.target.closest('li').id === String(answer)) {
-    console.log(true);
-  }
-}
-
-function AnswersList({ answersArray, answer }) {
+function AnswersList({ answersArray, answer, checkAnswer }) {
   const listItems = answersArray.map(element => <AnswerItem text={element.name} id={element.id} />);
   return (
     <ul className='answer-list' onClick={(event) => checkAnswer(event, answer)}>{ listItems }</ul>
