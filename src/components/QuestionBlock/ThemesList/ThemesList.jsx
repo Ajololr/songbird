b10c2232df3c2db62efaaa739cdf6c8f37f0ca35
Ilya Androsov <ilya_androsov@icloud.com>
2020-02-13 22:27:51 +0300

    Revert "feat: add responsive design"
    This reverts commit 8a7c185de80ae6edf0bf511f041fe65679219bff.

--- src/components/QuestionBlock/ThemesList/ThemesList.jsx
+++ src/components/QuestionBlock/ThemesList/ThemesList.jsx
@@ -8,9 +8,6 @@ function ThemesList() {
       <ThemeItem themeName={'Разминка'}/>
       <ThemeItem themeName={'Разминка'}/>
       <ThemeItem themeName={'Разминка'}/>
-      <ThemeItem themeName={'Разминка'}/>
-      <ThemeItem themeName={'Разминка'}/>
-      <ThemeItem themeName={'Разминка'}/>
     </ul>
   )
 }
